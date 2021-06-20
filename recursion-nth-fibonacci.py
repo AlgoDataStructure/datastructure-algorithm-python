7141e51bc0fb67fcae623d66e1dc6858aafcdf78
Sriraman Vellingiri <sriramanvellingiri@gmail.com>
2021-06-20 19:30:56 +0530

    fixes(recursion/nth-fibonacci): fibonacci

--- recursion-nth-fibonacci.py
+++ recursion-nth-fibonacci.py
@@ -2,13 +2,14 @@
 # O(n) time | O(1) space
 def getNthFib(n):
 	defaultArray = [0,1]
-	count = 3
-	while count <= n:
-		sumNo = defaultArray[0] + defaultArray[1]
-		defaultArray[0] = defaultArray[1]
-		defaultArray[1] = sumNo
-		count = count + 1
+
 	if n == 1:
 		return defaultArray[0]
 	else:
+		count = 3
+		while count <= n:
+			sum = defaultArray[0] + defaultArray[1]
+			defaultArray[0] = defaultArray[1]
+			defaultArray[1] = sum
+			count = count + 1
 		return defaultArray[1]
